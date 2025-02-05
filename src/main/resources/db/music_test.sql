46fb3a6eec499da5586b198c36afb0129ce71ec8
yisroel.yue <39448845+yisroelyue@users.noreply.github.com>
2025-02-05 11:38:38 +0800

    init

---
+++ src/main/resources/db/music_test.sql
@@ -0,0 +1,15 @@
+INSERT INTO linxidb.music
+(id, fileName, filePath, lyricPath, icPath, artist, album, genre, duration, releaseDate, fileSize, createdAt, updatedAt)
+VALUES(11, '孙燕姿-天黑黑', '/20250118/孙燕姿-天黑黑.mp3', '/20250118/孙燕姿-天黑黑.lrc', '/20250118/孙燕姿-天黑黑.mp3.Cover.jpg', '孙燕姿', '孙燕姿', '未知', '0', '1970-01-01', 0, '2025-01-18 11:53:49', '2025-01-31 08:45:36');
+INSERT INTO linxidb.music
+(id, fileName, filePath, lyricPath, icPath, artist, album, genre, duration, releaseDate, fileSize, createdAt, updatedAt)
+VALUES(12, '孙燕姿-我怀念的', '/20250118/孙燕姿-我怀念的.mp3', '/20250118/孙燕姿-我怀念的.lrc', '', '孙燕姿', '未知', '未知', '0', '1970-01-01', 0, '2025-01-18 11:53:49', '2025-01-18 11:56:14');
+INSERT INTO linxidb.music
+(id, fileName, filePath, lyricPath, icPath, artist, album, genre, duration, releaseDate, fileSize, createdAt, updatedAt)
+VALUES(14, '邓寓君(等什么君)-春庭雪', '/20250118/邓寓君(等什么君)-春庭雪.mp3', '/20250118/邓寓君(等什么君)-春庭雪.lrc', '', '邓寓君(等什么君)', '未知', '未知', '0', '1970-01-01', 0, '2025-01-18 11:53:49', '2025-01-18 11:56:14');
+INSERT INTO linxidb.music
+(id, fileName, filePath, lyricPath, icPath, artist, album, genre, duration, releaseDate, fileSize, createdAt, updatedAt)
+VALUES(15, '镜予歌&喧笑&陈亦洺-晚夜微雨问海棠', '/20250118/镜予歌&喧笑&陈亦洺-晚夜微雨问海棠.mp3', '/20250118/镜予歌&喧笑&陈亦洺-晚夜微雨问海棠.lrc', '', '镜予歌&喧笑&陈亦洺', '未知', '未知', '0', '1970-01-01', 0, '2025-01-18 11:53:49', '2025-01-18 11:56:14');
+INSERT INTO linxidb.music
+(id, fileName, filePath, lyricPath, icPath, artist, album, genre, duration, releaseDate, fileSize, createdAt, updatedAt)
+VALUES(16, '阿YueYue&戾格&小田音乐社-沈园外', '/20250118/阿YueYue&戾格&小田音乐社-沈园外.mp3', '/20250118/阿YueYue&戾格&小田音乐社-沈园外.lrc', '', '阿YueYue&戾格&小田音乐社', '未知', '未知', '0', '1970-01-01', 0, '2025-01-18 11:53:49', '2025-01-18 11:56:14');
---
+++ src/main/resources/db/sheet_test.sql
@@ -0,0 +1,21 @@
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(1, '孙燕姿的音乐专辑', '/sheet-ic/u=2825685220,3849949765&fm=253&fmt=auto&app=120&f=JPEG.webp', 'A collection of the top 40 hits of the year.', 1, '11,12', '2025-01-17 13:26:29', '2025-01-23 07:18:28');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(2, '国风精选', '/sheet-ic/微信图片_20250126191047.jpg', 'All-time greatest rock hits.', 2, '13,14,15,16', '2025-01-17 13:26:29', '2025-01-23 07:18:28');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(3, '氛围感钢琴曲', '/sheet-ic/微信图片_20250126191105.jpg', 'Relaxing and smooth jazz tracks.', 3, '3,8', '2025-01-17 13:26:29', '2025-01-26 11:05:56');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(4, '超好听的粤语歌曲合集', '/sheet-ic/微信图片_20250126191039.jpg', 'Best classical music for studying.', 4, '4,9', '2025-01-17 13:26:29', '2025-01-26 11:05:56');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(5, '宫崎骏纯音乐合集', '/sheet-ic/微信图片_20250126191032.jpg', 'Classic hip-hop tracks from the 90s.', 5, '5,10', '2025-01-17 13:26:29', '2025-01-26 11:05:56');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(6, '民谣精选', '/sheet-ic/微信图片_20250126191112.jpg', 'Upbeat pop songs to get the party started!', 6, '1,6', '2025-01-17 13:26:29', '2025-01-26 11:05:56');
+INSERT INTO linxidb.music_sheet
+(id, name, icUrl, description, userId, musicIds, createdAt, updatedAt)
+VALUES(7, '【beyongd】共你迎接光辉岁月', '/sheet-ic/微信图片_20250126191021.jpg', 'Best indie tracks for a laid-back vibe.', 7, '3,6,7', '2025-01-17 13:26:29', '2025-01-26 11:05:57');
